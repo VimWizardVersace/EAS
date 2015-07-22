c6f3d43dd827e22219639c8314a2a451180b8f52
roastmaster <tripiace@yahoo.com>
2015-07-22 14:40:02 -0400

    script that initializes nova compute node with given image

---
+++ scripts/worker_node_init.py
@@ -0,0 +1,53 @@
+# This file contains functions that automate the process for uploading, starting, and creating worker nodes
+# in glance.  Dependencies include python glance-client, our proprietary state of the art virtual machine image 
+# (courtesy of joe), and python nova.  
+
+from subprocess import call, Popen, PIPE
+from novaclient import client
+from keystoneclient import session
+from keystoneclient.auth.identity import v2
+from glanceclient import Client
+# using glance, upload a new image to the cloud (this is assuming it already isn't there [it should already be there])
+#
+def upload_new_image(Glance_Client, ImageLoc, Local=True):
+	
+	if Local:
+		
+		new_image = Glance_Client.images.create(name="TransBurst", disk_format="raw",
+										 container_format="bare", is_public=True)
+
+		new_image.update(data=open(ImageLoc,'rb'))
+		
+		return new_image
+
+
+	else:
+		"ayylmao"
+
+# after the image is uploaded, you will need to boot it with nova
+# first create a nova servergroup
+# load the image into the server
+#
+def activate_image(Nova_Client, ImageID, ServerName="myserver", Flavor=2):
+	nova_client.servers.create(ServerName, ImageID, Flavor)
+	nova_client.servers.start(ServerName)
+
+
+# main is used for testing
+#
+if __name__ == "__main__":
+
+	auth = v2.Password(auth_url="http://10.0.2.15:35357/v2.0", username="admin", password="light", tenant_name="admin")
+
+	sess = session.Session(auth=auth)
+
+	nova_client = client.Client("2.26.0", session=sess)
+
+	# NOTE: try the swift auth token
+	# can be found via command "swift stat -v"
+
+	glance_client = Client('1', endpoint="http://10.131.69.112:9292", token="8520212cd1a34b39b0e9d8d475144abb")
+
+	image = upload_new_image(glance_client, "gather.raw", True)
+	activate_image(nova_client, image.id, "test server", 2)
+
