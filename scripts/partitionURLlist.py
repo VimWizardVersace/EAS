eb484b59baf090dedffcf285f95c41193fd82976
roastmaster <tripiace@yahoo.com>
2015-07-22 14:31:30 -0400

    update partitionURLlist.py to use swift curl commands

--- scripts/partitionURLlist.py
+++ scripts/partitionURLlist.py
@@ -1,6 +1,7 @@
 import time
 import operator
 import paramiko
+from subprocess import call, PIPE, Popen, check_output
 
 def find_time_until_deadline(deadline):
 	#WARNING:  Time must be formatted the same as pattern
@@ -12,34 +13,25 @@ def find_time_until_deadline(deadline):
 		print "bruh give us a deadline in the form of MM/DD/YYYY HH:MM:SS"
 	return epoch - time.time()
 
-def PartitionURLlist(deadline, bitrate, URL):
-	# spawn an ssh client to connect to local cloud
+def PartitionURLlist(deadline, bitrate, URL, AUTH):
+	# use curl to connect to the object storage
 	# to discover the sizes of the videos to xcode
-	#	
-	client = paramiko.SSHClient()
-	client.load_system_host_keys()
-	try:
-		client.connect(URL[0], URL[1], URL[2], URL[3])
-	except:
-		print "invalid url"
-	print "connection to local cloud successful"
-
-	# travel to the appropriate directory and store file names + info
 	# 
-	(stdin, stdout, stderr) = client.exec_command('du -a ~/Videos')
-	filenames = stdout.read().split('\n')
-	filenames = [token.split('\t') for token in filenames]
-	client.close()	
-	
-	# delete the "total" footer and strip empty list
-	#
-	del filenames[-1]
-	del filenames[-1]
+	# note: authtoken may randomly change???
 
+	stdout = check_output(["curl", URL+"?format=json", "-X", "GET", "-H" , "X-Auth-Token:"+AUTH])
+	json_objs = stdout.split('{')
+	json_objs = [token.split(' ') for token in json_objs]
+
+	# get rid of garbage
+	#
+	del json_objs[0]
+	
 	# use a dictionary comprehension to assemble a size: filename map
 	#
+	file_size_dict = dict()
 	try:
-		file_size_dict = {token[1]: int(token[0]) for token in filenames}
+		file_size_dict = {token[7] : int(token[5].strip(",")) for token in json_objs}
 	except IndexError:
 		print "error (IndexError):  tried to ls on the local cloud to get file sizes.  things are formatted weird."
 
@@ -75,7 +67,14 @@ if __name__ == "__main__":
 	# URL[3] = password for VM
 	# This is neccesary to ssh into it and find their file sizes
 
-	test_url = ['10.131.77.64', 3022, 'user', 'pass']
-	test_deadline = '06/15/2015 16:10:09'
-	test_bitrate = 1024 * 0.5
-	PartitionURLlist(test_deadline, test_bitrate, test_url)
+	test_url = "http://10.131.69.121:8080/v1/AUTH_1eacdf40e54e4d72b1b1096e82d3bbe3/videos"
+	test_auth = "255d997d9321454fa3e2d454dc641cb1"
+	test_deadline = '07/15/2015 16:10:09'
+	test_bitrate = 1024 * 0
+	PartitionURLlist(test_deadline, test_bitrate, test_url, test_auth)
+	#stdout = check_output(["curl", test_url+"?format=json", "-X", "GET", "-H" , "X-Auth-Token:"+test_auth])
+	#print stdout
+
+
+
+
