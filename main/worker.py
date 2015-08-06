ab619484ef9273dfa4730a9bb8e7cfb48763d576
hjorne <joeh19@gmail.com>
2015-08-06 14:35:38 -0400

    Added threading to worker

--- main/worker.py
+++ main/worker.py
@@ -1,18 +1,33 @@
 from flask import Flask, request
+from threading import Thread
 import os
 
-
-def init():
-    global app
-    app = Flask(__name__)
-    app.config['UPLOAD_FOLDER'] = '/home/joe/tmp/'
-    app.run(host='0.0.0.0', port=5000, debug=True)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '~/tmp'
 
 
 @app.route("/config", methods=['POST'])
 def config():
     f = request.files['file']
-    f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], 'file_list'))
+    Thread(target=process)
     return ''
 
-init()
+def grab_file(filename):
+    """ Download file from swift """
+    pass
+
+
+
+
+
+def process():
+    file_list = open('file_list', 'r+')
+    for filename in file_list:
+        grab_file(filename)
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
